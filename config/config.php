27d67e4aad7ef569104dc3ed9bb71e8bf1d3b50d
abdi majiid haji <najacade23456@gmail.com>
2025-09-03 09:17:12 +0300

    change db connection to the production

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 // Update these for your environment
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql://root:SvwPNIUVdRzoalQEJJDdylbsSjyWKocn@hopper.proxy.rlwy.net:11670/railway');
 define('DB_NAME', 'bookstore');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'SvwPNIUVdRzoalQEJJDdylbsSjyWKocn');
 
 // Set this to your domain root, e.g. 'https://example.com' (no trailing slash)
-define('BASE_URL', '/bookstore');
+define('BASE_URL', 'mysql://root:SvwPNIUVdRzoalQEJJDdylbsSjyWKocn@mysql.railway.internal:3306/railway');
 
 // File upload base (relative to this repo root). Public path is /public/assets/uploads/covers
 define('UPLOAD_DIR', __DIR__ . '/../public/assets/uploads/covers/');
